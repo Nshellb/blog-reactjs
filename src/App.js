f161eb7d2c4faea794f3a5aaa59eac56a82a56b7
Nshell <nshellb@gmail.com>
2022-04-28 09:04:35 +0900

    04282022 Array, Object state 데이터 수정 Deep copy 개념

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ function App() {
   let [title, title_modify] = useState(['맛있는 떡볶이', '35년 전통 국수', 'Vue.js vs React']);
   let [like, like_modify] = useState(0);
 
+  function title_modify_f() {
+    // developer coding 
+    var newArray = [...title]; // title의 복사본 생성
+    newArray[0] = '순대와 떡볶이'; // 수정할 값 변경
+    title_modify( newArray );
+
+    // hard coding
+    // title_modify( ['순대와 떡볶이', '35년 전통 국수', 'Vue.js vs React'] );
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ title_modify_f }>버튼</button>
       <div className="list">
         <h3> { title[0] } <span onClick={ () => { like_modify(like + 1) } }>👍</span> { like } </h3>
         <p>4월 26일 발행</p>
